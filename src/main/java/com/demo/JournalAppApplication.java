f2531dc25db51662c1da2cb05bceeaca50b94449
Vivek Satao <viveksatao39@gmail.com>
2025-12-29 15:19:03 +0530

    Annotation in main method added

--- src/main/java/com/demo/JournalAppApplication.java
+++ src/main/java/com/demo/JournalAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-
+@EnableMongoRepositories(basePackages = "com.demo.repository")
 public class JournalAppApplication {
 
 	public static void main(String[] args) {
